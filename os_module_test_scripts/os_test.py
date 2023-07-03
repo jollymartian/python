7a70466a367520c473eec94b6abca173ca37f71a
jollymartian <63038849+jollymartian@users.noreply.github.com>
2023-07-03 12:18:19 +0100

    Update os_test.py

--- os_module_test_scripts/os_test.py
+++ os_module_test_scripts/os_test.py
@@ -3,4 +3,12 @@ import os
 #Pulls back an errorlevel into the x variable. 0 for success, 1 for failure
 x = os.system("for /f \"tokens=1 delims=-\" %a in ('hostname') do @echo %a")
 print(x)
-input("Press enter to quit...")
+
+#Use if loop to evaluate errorlevel
+if x == 0:
+    var = "Successful"
+else:
+    var = "Unsuccessful"
+
+print(var)
+input()
