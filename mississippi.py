aaaea2859bb0024f59aaae6636a9733b5acaf295
jollymartian <63038849+jollymartian@users.noreply.github.com>
2023-07-03 11:52:31 +0100

    Add files via upload

---
+++ mississippi.py
@@ -0,0 +1,10 @@
+#Import time module to use sleep
+import time
+
+#Create for loop to loop through 0-5 and print Mississippi each time
+for i in range(5):
+    print("Mississippi")
+    #Sleep for 1 second
+    time.sleep(1)
+#Print final message
+print("Ready or not here I come!")
