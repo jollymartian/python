360a46d94f1d9b111b39340996c7cdf8749200e7
jollymartian <63038849+jollymartian@users.noreply.github.com>
2023-07-03 19:50:04 +0100

    Update mississippi.py

--- mississippi.py
+++ mississippi.py
@@ -1,10 +1,14 @@
-#Import time module to use sleep
+#Import enitre time module to use sleep
 import time
+#Import sleep from time to call directly
+#from time import sleep
 
 #Create for loop to loop through 0-5 and print Mississippi each time
 for i in range(5):
     print("Mississippi")
     #Sleep for 1 second
     time.sleep(1)
+    #If direct
+    #sleep(1)
 #Print final message
 print("Ready or not here I come!")
