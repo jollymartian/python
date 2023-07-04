771733f8d6b7dbdb49bbfd18ebbf6b0fcbc1175d
jollymartian <63038849+jollymartian@users.noreply.github.com>
2023-07-04 12:27:04 +0100

    Create pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,20 @@
+#Script to return height of pyramid against full layers of blocks
+
+blocks = int(input("Enter the number of blocks: "))
+
+#Set top layer to one block
+height = 0
+number_in_layer = 0
+blocks -= 0
+
+#Loop using blocks variable
+while blocks > 0:
+    
+    if blocks - number_in_layer < 0:
+        break
+    else:
+        number_in_layer += 1
+        height += 1
+        blocks -= number_in_layer
+    
+print(f"The height of the pyramid: {height}")
