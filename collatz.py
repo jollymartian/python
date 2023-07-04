a7fc1983baa565284231de5a8bfef98e2c0002f1
jollymartian <63038849+jollymartian@users.noreply.github.com>
2023-07-04 18:52:16 +0100

    Create collatz.py

---
+++ collatz.py
@@ -0,0 +1,21 @@
+while True:
+    steps = 0
+    #1
+    c0 = int(input("Enter number: "))
+    while c0 != 1:
+        steps += 1
+        #2
+        if c0 % 2 == 0:
+            c0 = c0 / 2
+    #3
+        else:
+            c0 = 3 * c0 + 1
+    #4
+        if c0 == 1:
+            print(int(c0))
+            break
+        else:
+            print(int(c0))
+    print(f"Steps: {steps}")
+    if c0 == 'q':
+        break
