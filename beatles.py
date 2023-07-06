da5c8a1e7c7b2c6bdfd9b379e8d965568615cbca
jollymartian <63038849+jollymartian@users.noreply.github.com>
2023-07-06 10:21:28 +0100

    Create beatles.py

---
+++ beatles.py
@@ -0,0 +1,34 @@
+# step 1
+beatles = []
+print("Step 1:", beatles)
+
+# step 2
+beatles.append('John Lennon')
+beatles.append('Paul McCartney')
+beatles.append('George Harrison')
+
+print("Step 2:", beatles)
+
+# step 3
+for member in beatles:
+    member = input("Enter new members: ")
+    beatles.append(member)
+    more = input("More? y/n: ")
+    if more == 'y':
+        continue
+    else:
+        break
+    
+print("Step 3:", beatles)
+
+# step 4
+del beatles[3]
+del beatles[3]
+print("Step 4:", beatles)
+
+# step 5
+beatles.insert(0, 'Ringo Starr')
+print("Step 5:", beatles)
+
+# testing list legth
+print("The Fab", len(beatles))
