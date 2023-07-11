e5a20a88d76d8e6a9ce22ffb77a3fcb4838866a1
jollymartian <63038849+jollymartian@users.noreply.github.com>
2023-07-11 20:45:21 +0100

    Add files via upload

---
+++ snippets/for_loops_strings.py
@@ -0,0 +1,37 @@
+#Iterate through strings
+#Integers and floats are not iteratable
+
+#Assign x with a string
+x = "hello"
+
+#Iterate through sting(x)
+for i in x:
+	#Print each letter in sting(x) on same line using end="" to overwrite \n default
+	print(i, end = "")
+
+#Seperate examples with newline
+print("\n")
+
+#Iterate through string(x)
+for i in x:
+	#print each letter in string(x) on newline
+	print(i)
+
+#Seperate examples with newline
+print("\n")
+
+#Assign long string to x
+x = "Hello, World! Do you like Python?"
+#Iterate through string(x)
+for i in x:
+	#Print each letter on same line as above example
+	print(i, end = "")
+
+#Seperate examples with newline
+print("\n")
+
+#Iterate through string(x)
+for i in x:
+	#Print each letter on newline
+	print(i)
+
