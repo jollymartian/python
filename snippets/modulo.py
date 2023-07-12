8ed5543421303ca189a4c369ee4c94c1c3e787d8
jollymartian <63038849+jollymartian@users.noreply.github.com>
2023-07-12 20:52:46 +0100

    Add files via upload

---
+++ snippets/modulo.py
@@ -0,0 +1,18 @@
+#Modulo is the remainder
+
+#EG: 4 into 11 - how mant time does 4 go into 11...
+#Twice, witht he reaminder 3
+#3 is the modulo
+x = 11 % 4
+print(x)
+
+#Any positive number that is less than the positive number to be modulo against is always hat number:
+
+x = 5 % 6
+#x = 5
+print(x)
+
+x = 5 % 464755895
+#x = 5
+print(x)
+
